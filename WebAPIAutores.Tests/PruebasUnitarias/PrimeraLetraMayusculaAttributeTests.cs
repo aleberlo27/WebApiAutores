3f8cb1a66594e63c93c82cec8870e0d126b11f8d
Alejandra Bernal <alejandrabernallopez1@gmail.com>
2024-06-24 14:58:04 +0200

    Pruebas Unitarias

---
+++ WebAPIAutores.Tests/PruebasUnitarias/PrimeraLetraMayusculaAttributeTests.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.DataAnnotations;
+using WebApiAutores.Validaciones;
+
+
+namespace WebAPIAutores.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class PrimeraLetraMayusculaAttributeTests
+    {
+        [TestMethod]
+        public void PrimeraLetraMinuscula_DevuelveError()
+        {
+            //PREPARACION
+            var primeraLetraMayuscula = new PrimeraLetraMayusculaAttribute();
+            var valor = "alejandra";
+            var valContext = new ValidationContext(new { Nombre = valor });
+
+            //EJECUCIÓN
+            var resultado = primeraLetraMayuscula.GetValidationResult(valor, valContext);
+
+            //VERIFICACIÓN (assert es una clase que me permite hacer verificaciones, si la verificacion no es satisfactoria arroja un error)
+            Assert.AreEqual("La primera letra debe ser mayúscula.", resultado.ErrorMessage);
+        }
+
+        
+        [TestMethod]
+        public void ValorNulo_NoDevuelveError()
+        {
+            //PREPARACION
+            var primeraLetraMayuscula = new PrimeraLetraMayusculaAttribute();
+            string valor = null;
+            var valContext = new ValidationContext(new { Nombre = valor });
+
+            //EJECUCIÓN
+            var resultado = primeraLetraMayuscula.GetValidationResult(valor, valContext);
+
+            //VERIFICACIÓN (assert es una clase que me permite hacer verificaciones, si la verificacion no es satisfactoria arroja un error)
+            Assert.IsNull(resultado);
+        }
+
+        [TestMethod]
+        public void ValorConPrimeraLetraMayuscula_NoDevuelveError()
+        {
+            //PREPARACION
+            var primeraLetraMayuscula = new PrimeraLetraMayusculaAttribute();
+            string valor = "Alejandra";
+            var valContext = new ValidationContext(new { Nombre = valor });
+
+            //EJECUCIÓN
+            var resultado = primeraLetraMayuscula.GetValidationResult(valor, valContext);
+
+            //VERIFICACIÓN (assert es una clase que me permite hacer verificaciones, si la verificacion no es satisfactoria arroja un error)
+            Assert.IsNull(resultado);  
+        }
+
+    }
+}
